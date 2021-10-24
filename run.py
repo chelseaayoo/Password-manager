705ff1c1c2423305a4d4c9a5236c9599035c4d2e
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-24 05:51:28 -0400

    create a new credential

---
+++ run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.8
+from credential import Credential
+def create_credential(email,password):
+    '''
+    Function to create a new credential
+    '''
+    new_credential = Credential(email,password)
+    return new_credential
