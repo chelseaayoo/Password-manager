1d4d570763a274316aff735c4f2ebf9abed4c814
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-24 04:40:03 -0400

    create delete credential method

--- credential.py
+++ credential.py
@@ -21,3 +21,10 @@ class Credential:
         '''
 
         Credential.credential_list.append(self)
+    def delete_credential(self):
+
+        '''
+        delete_credential method deletes a saved credential from the credential_list
+        '''
+
+        Credential.credential_list.remove(self)
