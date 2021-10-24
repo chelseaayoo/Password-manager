2255d1955c6c50159ae4f0d3d7ab7923eb7c6f3c
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-24 04:40:46 -0400

    check if we can delete a credential

--- credential_test.py
+++ credential_test.py
@@ -58,6 +58,17 @@ class TestCredential(unittest.TestCase):
             test_credential = Credential("test@user.com","chelsea2021") # new credential
             test_credential.save_credential()
             self.assertEqual(len(Credential.credential_list),2)
+    # More tests above
+    def test_delete_credential(self):
+            '''
+            test_delete_credential to test if we can remove a credential from our credential list
+            '''
+            self.new_credential.save_credential()
+            test_credential = Credential("test@user.com","chelsea2021") # new credential
+            test_credential.save_credential()
+
+            self.new_credential.delete_credential()# Deleting a credential object
+            self.assertEqual(len(Credential.credential_list),1)
 
 
 if __name__ == '__main__':
