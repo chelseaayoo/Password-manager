e440eca81aa84a5bd50d15b6d5c35efab3040b95
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-24 05:56:48 -0400

    delete a credential

--- run.py
+++ run.py
@@ -11,3 +11,8 @@ def save_credentials(credential):
     Function to save credential
     '''
     credential.save_credential()
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()
