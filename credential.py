3be6d7bc6c9c5088212b68728e36650344ae54fd
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-24 03:23:52 -0400

    create credential_list variable

--- credential.py
+++ credential.py
@@ -1,6 +1,13 @@
 class Credential:
     """
-    Class that generates new instances of credentials
+    Class that generates new instances of credentials.
     """
 
-    pass
+    credential_list = [] # Empty credential list
+
+    def __init__(self,email,password):
+
+      # docstring removed for simplicity
+
+        self.email = email
+        self.password = password
