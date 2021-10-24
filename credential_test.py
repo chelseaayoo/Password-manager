ccca05fc96533a517b0c16d64451b909f04a8388
anipherchelsea <chelsea.ayoo@student.moringaschool.com>
2021-10-24 03:57:51 -0400

    test if our objects are being instantiated correctly

--- credential_test.py
+++ credential_test.py
@@ -1,5 +1,5 @@
 import unittest # Importing the unittest module
-from credential import Credential # Importing the contact class
+from credential import Credential # Importing the credential class
 
 class TestCredential(unittest.TestCase):
 
@@ -9,3 +9,23 @@ class TestCredential(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+    # Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_credential = Credential("chelsea.ayoo@student.moringaschool.com","chelsea2021") # create credential object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_credential.email,"chelsea.ayoo@student.moringaschool.com")
+        self.assertEqual(self.new_credential.password,"chelsea2021")
+
+
+if __name__ == '__main__':
+    unittest.main()
